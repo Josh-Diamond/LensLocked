ff652f684e2e96a9c8598d53b9ceaed8a1ba66de
Josh Diamond <diamondroad91@gmail.com>
2022-01-12 16:02:12 -0700

    templates

---
+++ cmd/exp/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Age int
+	Meta struct {
+		Visits int
+	}
+}
+
+type UserMeta struct {
+	Visits int
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	user := User{
+		Name: "Mr. Diamond",
+		Age: 111,
+		Meta: UserMeta{
+			Visits: 4,
+		},
+	}
+
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+}
