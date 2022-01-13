c325e81cdc5d126714646ad5aff588e068456dbd
Josh Diamond <diamondroad91@gmail.com>
2022-01-12 17:59:06 -0700

    faq template

--- main.go
+++ main.go
@@ -36,8 +36,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset-utf-8")
-	fmt.Fprint(w, "<h1>FAQ Page!</h1>")
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
 func main() {
